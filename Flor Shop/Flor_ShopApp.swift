1c663ee9d6c4b044aca3c8e6e52315b174ee9ad3
AngelFox24 <curilaurente@gmail.com>
2023-04-12 07:12:08 -0500

    Initial Commit

---
+++ Flor Shop/Flor_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flor_ShopApp.swift
+//  Flor Shop
+//
+//  Created by Angel Curi Laurente on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Flor_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
