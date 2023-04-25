62b51211630bfba066c1ec737a0ceff2f2d8fbfa
AngelFox24 <curilaurente@gmail.com>
2023-04-24 20:31:32 -0500

    CardView Actualizado
    Se cambio el diseño del card view

--- Flor Shop/View/Components/ProductoCardView.swift
+++ Flor Shop/View/Components/ProductoCardView.swift
@@ -22,36 +22,49 @@ struct ProductoCardView: View {
                     .frame(width: size,height: size)
                     .cornerRadius(20.0)
                 VStack {
-                    Text(producto.name)
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .padding(.bottom,10)
+                    HStack {
+                        Text(producto.name)
+                            //.lineLimit(2)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .padding(.horizontal,5)
+                        Spacer()
+                    }
                     Spacer()
-                    Text(producto.expiredate)
-                        .padding(.top,10)
+                    HStack {
+                        Text(String("\(producto.expiredate)"))
+                        //.frame(width: 55, height: 20)
+                            .padding(.vertical,10)
+                            .padding(.horizontal,10)
+                            //.fontWeight(.bold)
+                            .foregroundColor(Color("color_background"))
+                            .background(Color("color_secondary"))
+                            .cornerRadius(20)
+                        Spacer()
+                        Text(String(producto.quantity))
+                        //.frame(width: 55, height: 20)
+                            .padding(.vertical,10)
+                            .padding(.horizontal,10)
+                            //.fontWeight(.bold)
+                            .foregroundColor(Color("color_background"))
+                            .background(Color("color_secondary"))
+                            .cornerRadius(20)
+                        Spacer()
+                        Text(String("S/. \(producto.price)"))
+                        //.frame(width: 55, height: 20)
+                            .padding(.vertical,10)
+                            .padding(.horizontal,10)
+                            //.fontWeight(.bold)
+                            .foregroundColor(Color("color_background"))
+                            .background(Color("color_secondary"))
+                            .cornerRadius(20)
+                    }
                 }
                 .padding(.vertical,10)
-                .multilineTextAlignment(.leading)
-                .frame(maxWidth: .infinity)
-                VStack {
-                    Text(String(producto.quantity))
-                    //.frame(width: 55, height: 20)
-                        .padding(.vertical,10)
-                        .padding(.horizontal,10)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("color_icons"))
-                        .background(Color("color_secondary"))
-                        .cornerRadius(10)
-                    Text(String("S/. \(producto.price)"))
-                    //.frame(width: 55, height: 20)
-                        .padding(.vertical,10)
-                        .padding(.horizontal,10)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("color_icons"))
-                        .background(Color("color_secondary"))
-                        .cornerRadius(10)
-                }
-                .padding(.trailing)
+                .padding(.trailing,10)
+                //.multilineTextAlignment(.leading)
+                //.frame(maxWidth: .infinity)
+                
             }
             .frame(maxWidth: .infinity, maxHeight: size)
             .background(Color("color_primary"))
@@ -60,18 +73,12 @@ struct ProductoCardView: View {
             
         }.onAppear{
             imageProductNetwork.getImage(url: (URL(string: producto.imageURL )!))
-            //imagencita=mueble.imagenRenderizada
-            //muebles.guardarImagenRenderizada(idMuebleInput: mueble.id, imagenInput: imagenMuebleNetwork.fotoMueble)
-            //mueble.imagenRenderizada = imagenMuebleNetwork.fotoMueble
-            //Task.sleep(nanoseconds: 3_000_000_000)
-            //muebles.muebles[mueble.id-1].imagenRenderizada = imagenMuebleNetwork.fotoMueble
-            
         }
     }
 }
 
 struct ProductoCardView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductoCardView(idProducto: "1",producto: ProductoModel(id: "0", expiredate: "29/03/2023", imageURL: "https://falabella.scene7.com/is/image/FalabellaPE/882760327_2?wid=240&hei=240&qlt=70&fmt=webp", name: "Mocasin", price: 78.90, quantity: 34), size: 120.0)
+        ProductoCardView(idProducto: "1",producto: ProductoModel(id: "0", expiredate: "29/03/2023", imageURL: "https://falabella.scene7.com/is/image/FalabellaPE/882760327_2?wid=240&hei=240&qlt=70&fmt=webp", name: "Use este modificador para diferenciar entre ciertas vistas seleccionables, como los valores posibles de un Picker o las pestañas de un TabView. Los valores de etiqueta pueden ser de cualquier tipo que se ajuste al protocolo Hashable.En el siguiente ejemplo, el bucle ForEach en el generador de vistas Picker itera sobre la enumeración Flavor.", price: 78.90, quantity: 34), size: 120.0)
     }
 }
