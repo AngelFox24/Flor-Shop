1945c2053fa50365924064f488025c1a74d70b6d
AngelFox24 <curilaurente@gmail.com>
2024-03-27 16:48:18 -0500

    Optimized

--- Flor Shop/Presentation/SideMenu/PointOfSale/ProductView/ViewModel/ProductViewModel.swift
+++ Flor Shop/Presentation/SideMenu/PointOfSale/ProductView/ViewModel/ProductViewModel.swift
@@ -13,13 +13,10 @@ class ProductViewModel: ObservableObject {
     @Published var searchText: String = ""
     @Published var primaryOrder: PrimaryOrder = .nameAsc
     @Published var filterAttribute: ProductsFilterAttributes = .allProducts
-    @Published var scrollToIndex: Int = 0
-    @Published var currentIndex: Int = 1
+    @Published var deleteCount: Int = 0
     
-    // TODO: Optimizar Lista
     private var currentPagesInScreen: [[Int]] = []
-    //-------------------------
-    //private var currentPage: Int = 1
+    private let maxPagesToLoad: Int = 3
     private var lastCarge: Int = 0
     private var cancellableSet = Set<AnyCancellable>()
     
@@ -29,52 +26,29 @@ class ProductViewModel: ObservableObject {
         self.getProductsUseCase = getProductsUseCase
         addSearchTextSuscriber()
     }
-    func fetchProducts(page: Int = 1) {
-        if page == 1 {
-            self.currentPagesInScreen = []
-            let productsNewCarge = self.getProductsUseCase.execute(seachText: searchText, primaryOrder: primaryOrder, filterAttribute: filterAttribute, page: page)
-            lastCarge = productsNewCarge.count
-            
-            
-            self.productsCoreData = productsNewCarge
-            self.currentPagesInScreen.append([page,lastCarge])
-        } else {
-            if lastCarge > 0 {
-                let productsNewCarge = self.getProductsUseCase.execute(seachText: searchText, primaryOrder: primaryOrder, filterAttribute: filterAttribute, page: page)
-                lastCarge = productsNewCarge.count
-                guard let lastPage = self.currentPagesInScreen.last?[0] else { return }
-                guard let firstPage = self.currentPagesInScreen.first?[0] else { return }
-                if lastPage + 1 == page {
-                    self.currentPagesInScreen.append([page, lastCarge])
-                    self.productsCoreData.append(contentsOf: productsNewCarge)
-                } else if firstPage - 1 == page {
-                    self.currentPagesInScreen.insert(contentsOf: [[page, lastCarge]], at: 0)
-                    self.productsCoreData.insert(contentsOf: productsNewCarge, at: 0)
-                }
+    func fetchProducts(page: Int = 1, nextPage: Bool = true) {
+        let productsNewCarge = self.getProductsUseCase.execute(seachText: searchText, primaryOrder: primaryOrder, filterAttribute: filterAttribute, page: page)
+        lastCarge = productsNewCarge.count
+        if lastCarge > 0 {
+            if nextPage {
+                self.currentPagesInScreen.append([page, lastCarge])
+                self.productsCoreData.append(contentsOf: productsNewCarge)
+            } else {
+                self.currentPagesInScreen.insert(contentsOf: [[page, lastCarge]], at: 0)
+                self.productsCoreData.insert(contentsOf: productsNewCarge, at: 0)
             }
         }
-    }
-    func fetchNextPageBK() {
-        //currentPage = currentPage + 1
-        //fetchProducts(page: currentPage)
-    }
-    func removeUnnecesaryItems() {
-        //let toRemove = self.productsCoreData.count - 60
-        //if toRemove > 0 {
-        self.productsCoreData.removeFirst()
-        self.scrollToIndex = self.currentIndex - 2
     }
     func fetchNextPage() {
         guard let firstCharge = self.currentPagesInScreen.first?[1] else { return }
         guard let lastPage = self.currentPagesInScreen.last?[0] else { return }
         fetchProducts(page: lastPage + 1)
         print("fetchNextPage: \(lastPage + 1)")
-        //self.currentPagesInScreen.append(lastPage + 1)
-        if self.currentPagesInScreen.count > 4 {
-            self.productsCoreData.removeFirst(firstCharge)
-            self.currentPagesInScreen.removeFirst()
-            if self.currentIndex - 2 - firstCharge > 0 {
-                self.scrollToIndex = self.currentIndex - 2 - firstCharge
+        if lastCarge > 0 {
+            if self.currentPagesInScreen.count > self.maxPagesToLoad {
+                self.deleteCount = self.deleteCount + firstCharge
+                self.productsCoreData.removeFirst(firstCharge)
+                self.currentPagesInScreen.removeFirst()
             }
         }
         print("CurrentPagesInScreen: \(currentPagesInScreen.description)")
@@ -82,18 +56,21 @@ class ProductViewModel: ObservableObject {
         print("ProductsCoreData: \(productsCoreData.count.description)")
     }
     func fetchPreviousPage() {
+        guard let firstCharge = self.currentPagesInScreen.first?[1] else { return }
         guard let firstPage = self.currentPagesInScreen.first?[0] else { return }
         guard let lastCharge = self.currentPagesInScreen.last?[1] else { return }
         let previousPage = firstPage - 1
         print("fetchPreviousPage: \(previousPage)")
         if previousPage >= 1 {
-            fetchProducts(page: previousPage)
-            //self.currentPagesInScreen.insert(contentsOf: [previousPage], at: 0)
-            if self.currentPagesInScreen.count > 4 {
-                self.productsCoreData.removeLast(lastCharge)
-                self.currentPagesInScreen.removeLast()
-                if self.currentIndex + 2 + firstPage <= self.productsCoreData.count {
-                    self.scrollToIndex = self.currentIndex + 2
+            fetchProducts(page: previousPage, nextPage: false)
+            if lastCarge > 0 {
+                if self.currentPagesInScreen.count > self.maxPagesToLoad {
+                    print("Se borrara espacios: \(self.deleteCount - firstCharge)")
+                    self.deleteCount = self.deleteCount - firstCharge
+                    self.productsCoreData.removeLast(lastCharge)
+                    self.currentPagesInScreen.removeLast()
+                } else {
+                    self.deleteCount = 0
                 }
             }
         }
